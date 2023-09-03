a18d8ae9b760eb5515434b6a6d3043cd150f547a
Krypter <91453436+Krypter93@users.noreply.github.com>
2023-09-03 00:54:49 -0400

    Update quotes.js

--- quotes.js
+++ quotes.js
@@ -8,19 +8,6 @@ let getQuote = ()=>{
      fetch('https://api.kanye.rest/')
     .then(response => response.json())
      .then(data => text.textContent = data.quote);
-
-    /* let xhr = new XMLHttpRequest;
-     xhr.open("GET", "https://api.kanye.rest/", true);
-     xhr.onload = ()=>{
-         if(xhr.status === 200){
-             text.textContent = xhr.responseText;
-         }else{
-             console.error(xhr.statusText);
-         }
-     };
-    xhr.send();*/
-
-    //alert("Kanye Loves You");
    }) 
 }
 
